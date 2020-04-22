d7194e7b88441c68b5cac9885e51d98f2b3dc281
Thiago Marinho <tgmarinho@gmail.com>
2020-04-22 17:27:41 -0400

    GoBarber Web - Estrutura das páginas - Estilos Globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Estrutura Inicial</h1>;
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>Estrutura Inicial</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
