19d518a22f378983a92b272a66e5c26fe1c13edd
Thiago Marinho <tgmarinho@gmail.com>
2020-05-24 10:59:23 -0400

    GoBarber Web - dashboard  - criando o header

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,20 +1,26 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  avatar_url: string;
+  name: string;
+}
+
 interface SignInCredencials {
   email: string;
   password: string;
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credencials: SignInCredencials): Promise<void>;
   signOut(): void;
 }
 
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
